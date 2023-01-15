61540d09c3f302c69b8d0381ff8cbc0950c7eb52
Alen Redek <alen@redek.me>
2023-01-15 23:31:30 +0100

    Bootstrap the application with a Standalone Component instead of NgModule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
+  standalone: true,
+  imports: [RouterModule, NxWelcomeComponent],
   selector: 'nx-angular-barbershop-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
