c3178e97e34b6a5e5a280ef9954c5dd7c4b86470
Alen Redek <alen@redek.me>
2023-01-15 11:48:43 +0100

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-angular-barbershop-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'nx-angular-barbershop';
+}
