7d9a47dabbae537e6ced9dd66e740d378136882a
Alen Redek <alen@redek.me>
2023-01-16 23:50:49 +0100

    Added lazy loaded route appointment

---
+++ src/app/appointment/appointment.routes.ts
@@ -0,0 +1,7 @@
+import { Route } from '@angular/router';
+
+import { AppointmentComponent } from './appointment.component';
+
+export const APPOINTMENT_ROUTES: Route[] = [
+  { path: '', component: AppointmentComponent },
+];
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,26 @@
+import { importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(
+      RouterModule.forRoot([
+        {
+          path: 'appointment',
+          loadChildren: () =>
+            import('src/app/appointment/appointment.routes').then(
+              (m) => m.APPOINTMENT_ROUTES,
+            ),
+        },
+        {
+          path: '',
+          pathMatch: 'full',
+          redirectTo: 'appointment',
+        },
+      ]),
+    ),
+  ],
+}).catch((err) => console.error(err));
