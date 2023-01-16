365fe598bcfdeb4d8459287fa5e351bfd11ec4a5
Alen Redek <alen@redek.me>
2023-01-16 23:29:21 +0100

    Removed TestBed from tests to simplify the code

--- src/app/appointment/appointment.component.spec.ts
+++ src/app/appointment/appointment.component.spec.ts
@@ -1,19 +1,10 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { AppointmentComponent } from './appointment.component';
 
 describe('AppointmentComponent', () => {
   let component: AppointmentComponent;
-  let fixture: ComponentFixture<AppointmentComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AppointmentComponent],
-    }).compileComponents();
-
-    fixture = TestBed.createComponent(AppointmentComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  beforeEach(() => {
+    component = new AppointmentComponent();
   });
 
   it('should create', () => {
