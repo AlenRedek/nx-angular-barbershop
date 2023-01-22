ffa82b20471924cc30136317706272b0288ae36b
Alen Redek <alen@redek.me>
2023-01-22 01:19:10 +0100

    Added styles for buttons

--- src/app/features/appointment/components/appointment-form/appointment-form.component.ts
+++ src/app/features/appointment/components/appointment-form/appointment-form.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'nx-angular-barbershop-appointment-form',
   standalone: true,
+  imports: [ButtonModule],
   templateUrl: './appointment-form.component.html',
   styleUrls: ['./appointment-form.component.scss'],
 })
