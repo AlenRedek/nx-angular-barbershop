4300e6142fddcdb222fcd9a29c6e097adeab188a
Alen Redek <alen@redek.me>
2023-01-22 18:00:38 +0100

    Added strictly typed reactive form

--- src/app/features/appointment/components/appointment-form/appointment-form.component.ts
+++ src/app/features/appointment/components/appointment-form/appointment-form.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { CalendarModule } from 'primeng/calendar';
 import { InputTextModule } from 'primeng/inputtext';
@@ -6,8 +12,28 @@ import { InputTextModule } from 'primeng/inputtext';
 @Component({
   selector: 'nx-angular-barbershop-appointment-form',
   standalone: true,
-  imports: [ButtonModule, CalendarModule, InputTextModule],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    ButtonModule,
+    CalendarModule,
+    InputTextModule,
+  ],
   templateUrl: './appointment-form.component.html',
   styleUrls: ['./appointment-form.component.scss'],
 })
-export class AppointmentFormComponent {}
+export class AppointmentFormComponent {
+  public appointmentForm = this.formBuilder.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    contactNumber: ['', Validators.required],
+    barber: ['', Validators.required],
+    service: ['', Validators.required],
+    date: ['', Validators.required],
+    time: ['', Validators.required],
+    price: ['', Validators.required],
+  });
+
+  public constructor(private readonly formBuilder: FormBuilder) {}
+}
