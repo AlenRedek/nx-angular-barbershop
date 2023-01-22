760b35f865f1eca27b30e47cc42bd200bdd9da99
Alen Redek <alen@redek.me>
2023-01-22 11:11:30 +0100

    Added PrimeNG directive for text field

--- src/app/features/appointment/components/appointment-form/appointment-form.component.ts
+++ src/app/features/appointment/components/appointment-form/appointment-form.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'nx-angular-barbershop-appointment-form',
   standalone: true,
-  imports: [ButtonModule],
+  imports: [ButtonModule, InputTextModule],
   templateUrl: './appointment-form.component.html',
   styleUrls: ['./appointment-form.component.scss'],
 })
