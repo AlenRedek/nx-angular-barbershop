6126476511108b0b09b337be3ea219af19ad3f04
Alen Redek <alen@redek.me>
2023-01-22 16:42:14 +0100

    Added PrimeNG directive for calendar

--- src/app/features/appointment/components/appointment-form/appointment-form.component.ts
+++ src/app/features/appointment/components/appointment-form/appointment-form.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { CalendarModule } from 'primeng/calendar';
 import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'nx-angular-barbershop-appointment-form',
   standalone: true,
-  imports: [ButtonModule, InputTextModule],
+  imports: [ButtonModule, CalendarModule, InputTextModule],
   templateUrl: './appointment-form.component.html',
   styleUrls: ['./appointment-form.component.scss'],
 })
