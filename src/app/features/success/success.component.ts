9e970b90c60b63c433a97dec0b5fafd54828d41b
Alen Redek <alen@redek.me>
2023-01-24 00:54:25 +0100

    Added new feature component Success

---
+++ src/app/features/success/success.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-angular-barbershop-success',
+  standalone: true,
+  templateUrl: './success.component.html',
+  styleUrls: ['./success.component.scss'],
+})
+export class SuccessComponent {}
