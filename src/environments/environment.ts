d237eeecfe6e6181064dd6f525a5a4c0ce14e86e
Alen Redek <alen@redek.me>
2023-02-04 12:57:25 +0100

    Added env comment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
+  // The API key is visible in the network tab anyway
   barberGifsApiUrl:
     'https://api.giphy.com/v1/gifs/search?q=barber&api_key=KeTn0RgXZQF8EDkUGgQmSaJYuWPEz5mI',
 };
