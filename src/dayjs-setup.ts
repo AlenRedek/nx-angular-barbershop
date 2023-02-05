2535d3cf7eae6cbe2b85e8263b86e993909925a8
Alen Redek <alen@redek.me>
2023-02-05 13:07:55 +0100

    Local time format could be displayed without the timezone plugin

--- src/dayjs-setup.ts
+++ src/dayjs-setup.ts
@@ -1,8 +1,6 @@
 import dayjs from 'dayjs';
 import isBetween from 'dayjs/plugin/isBetween';
 import utc from 'dayjs/plugin/utc';
-import timezone from 'dayjs/plugin/timezone';
 
 dayjs.extend(isBetween);
 dayjs.extend(utc);
-dayjs.extend(timezone);
