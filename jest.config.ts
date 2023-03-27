48cb10df40471ba9001737044a2c9cdadcd72c4e
Alen Redek <alen@redek.me>
2023-03-27 20:38:11 +0200

    Run NX migration scripts

--- jest.config.ts
+++ jest.config.ts
@@ -3,12 +3,7 @@ export default {
   displayName: 'nx-angular-barbershop',
   preset: './jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-      stringifyContentPathRegex: '\\.(html|svg)$',
-    },
-  },
+  globals: {},
   coverageDirectory: './coverage/nx-angular-barbershop',
   coverageThreshold: {
     global: {
@@ -19,7 +14,13 @@ export default {
     },
   },
   transform: {
-    '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
+    '^.+\\.(ts|mjs|js|html)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$',
+      },
+    ],
   },
   transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
   snapshotSerializers: [
